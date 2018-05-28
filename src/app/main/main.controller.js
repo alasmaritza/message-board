94830930181ecf88eb0d5b390a09ac3cd2adf8da
Maritza Alas <alas.maritza.j@gmail.com>
2018-05-27 22:31:03 -0700

    added other form fields

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -13,7 +13,15 @@ export class MainController {
     });
   }
 postMessage() {
-  this.$http.post('http://localhost:5000/api/message', {msg:this.message});
+  this.$http.post('http://localhost:5000/api/message', {
+    resourceType: this.resource,
+    locationName: this.location,
+    address: this.address,
+    phone: this.phone,
+   // email: this.email,
+    website: this.website,
+    msg:this.message
+  });
 }
 
 }
