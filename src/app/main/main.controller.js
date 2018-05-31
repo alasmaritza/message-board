a53444260f7d464f93a48f64ccfc147c25f7bcd2
Maritza Alas <alas.maritza.j@gmail.com>
2018-05-31 13:49:08 -0700

    add category

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,27 +1,41 @@
 export class MainController {
-  constructor ($http ) {
+  constructor($http) {
     'ngInject';
-      this.$http = $http;
-      this.getMessages();
+    this.$http = $http;
+    this.getMessages();
   }
 
   getMessages() {
     var vm = this;
-    this.$http.get('http://localhost:5000/api/message').then(function(result){
-        console.log(result);
-        vm.messages = result.data;
+    this.$http.get('http://localhost:5000/api/message').then(function (result) {
+      console.log(result);
+      vm.messages = result.data;
     });
   }
-postMessage() {
-  this.$http.post('http://localhost:5000/api/message', {
-    resourceType: this.resource,
-    locationName: this.location,
-    address: this.address,
-    phone: this.phone,
-   // email: this.email,
-    website: this.website,
-    msg:this.message
-  });
-}
+
+  getCategories() {
+    var vm = this;
+    this.$http.get('http://localhost:5000/api/category').then(function (result) {
+      console.log(result);
+      vm.categories = result.data;
+    });
+  }
+  postMessage() {
+    this.$http.post('http://localhost:5000/api/message', {
+      resourceType: this.resource,
+      locationName: this.location,
+      address: this.address,
+      phone: this.phone,
+      // email: this.email,
+      website: this.website,
+      msg: this.message
+    });
+  }
+
+  postCategory() {
+    this.$http.post('http://localhost:5000/api/category', {
+      categoryName: this.category
+    });
+  }
 
 }
