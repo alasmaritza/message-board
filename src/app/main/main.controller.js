2626ca0517fa12b8360401d8ed163ca52c9464db
Maritza Alas <alas.maritza.j@gmail.com>
2018-06-03 18:18:55 -0700

    login and logout working

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,14 +1,16 @@
 export class MainController {
-  constructor($http) {
+  constructor($http, $scope) {
     'ngInject';
     this.$http = $http;
+    this.$scope = $scope;
     this.getMessages();
+    this.getCategories();
+    this.state = 'CA';
   }
 
   getMessages() {
     var vm = this;
     this.$http.get('http://localhost:5000/api/message').then(function (result) {
-      console.log(result);
       vm.messages = result.data;
     });
   }
@@ -16,7 +18,6 @@ export class MainController {
   getCategories() {
     var vm = this;
     this.$http.get('http://localhost:5000/api/category').then(function (result) {
-      console.log(result);
       vm.categories = result.data;
     });
   }
@@ -24,11 +25,11 @@ export class MainController {
     this.$http.post('http://localhost:5000/api/message', {
       resourceType: this.resource,
       locationName: this.location,
-      address: this.address,
+      address: {address:this.address, city: this.city, state: this.state, zip: this.zip},
       phone: this.phone,
-      // email: this.email,
       website: this.website,
-      msg: this.message
+      msg: this.message,
+      created: new Date
     });
   }
 
